3eea39e57b7a9c1f23a2ed8400640c7f6cb430cf
Anton Jalalipour <chad.jalalipour@gmail.com>
2022-01-01 21:18:53 -0500

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import Converter from "./components/Converter";
+import NewsFeed from "./components/NewsFeed";
+function App() {
+  return (
+    <div className="app">
+      <div className="app__wrapper">
+        <Converter />
+        <NewsFeed />
+      </div>
+    </div>
+  );
+}
+
+export default App;
