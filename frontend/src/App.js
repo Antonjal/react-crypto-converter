1f5c90d491b0fd9e2acd0a67fb8ebd52fc78268a
Anton Jalalipour <anton.jalalipour@gmail.com>
2022-01-05 20:35:52 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import Converter from "./components/Converter";
 import NewsFeed from "./components/NewsFeed";
+import Explain from "./components/Explain";
+
 function App() {
   return (
     <div className="app">
@@ -7,6 +9,7 @@ function App() {
         <Converter />
         <NewsFeed />
       </div>
+      <Explain />
     </div>
   );
 }
