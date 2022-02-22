21b7d8fe34cd2fdfadf779277865bbdb9ff4447f
Anton Jalalipour <anton.jalalipour@gmail.com>
2022-02-22 18:55:04 -0500

    Update ExchangeRate.js

--- frontend/src/components/ExchangeRate.js
+++ frontend/src/components/ExchangeRate.js
@@ -2,7 +2,7 @@ function ExchangeRate({exchangedData}) {
     return (
       <div className="exchange__rate">
         <h3>Exchange Rate</h3> 
-        <h1>{exchangedData.exchangeRate}</h1>
+        <h1>{exchangedData.exchangeRate.toFixed(2)}</h1>
         <p>{exchangedData.primaryCurrency} to {exchangedData.secondaryCurrency}</p>
       </div>
     );
